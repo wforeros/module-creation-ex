f5fed2ac9ea02b1ea77dc9a10d6cc268cb72cbad
Wilson Forero <wilson.forero@trespimedios.co>
2023-01-13 16:31:36 -0500

    F Commit

---
+++ lib/index.d.ts
@@ -0,0 +1,3 @@
+import { add } from './math/add';
+import { subtract } from './math/subtract';
+export { add, subtract };
---
+++ src/index.ts
@@ -0,0 +1,6 @@
+import { add } from './math/add'
+import { subtract } from './math/subtract'
+export {
+  add,
+  subtract
+}
